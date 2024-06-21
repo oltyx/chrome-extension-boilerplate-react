3f764bbe433021896f6df3e72bea85c7122dce1b
Mihai Macarie <yo2mno@yahoo.ro>
2024-06-21 23:39:45 +0200

    added keyword check

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -8,6 +8,8 @@ printLine("Using the 'printLine' function from the Print Module");
 let swiping = false;
 let swipeInterval;
 
+let keywords = ''
+
 function swipeRight() {
     const xpath = "//button[.//span[contains(@class, 'Hidden') and text()='Like']]";
     const likeButton = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
@@ -19,15 +21,40 @@ function swipeRight() {
             cancelable: true
         });
         likeButton.dispatchEvent(event);
-        console.log("Swiped right");
+
     } else {
         console.log("Like button not found");
+    }
+}
+
+
+function swipeLeft() {
+    const xpath = "//button[.//span[contains(@class, 'Hidden') and text()='Nope']]";
+    const nopeButton = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    if (nopeButton) {
+        const event = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true
+        });
+        nopeButton.dispatchEvent(event);
+
+    } else {
+        console.log("Like button not found");
+    }
+}
+const swiper = () => {
+    if (checkKeywords()) {
+        swipeRight()
+    } else {
+        swipeLeft()
     }
 }
 function startSwiping() {
     if (!swiping) {
         swiping = true;
-        swipeInterval = setInterval(swipeRight, 2000);
+        swipeInterval = setInterval(swiper, 2000);
     }
 }
 
@@ -38,8 +65,73 @@ function stopSwiping() {
     }
 }
 
+function getDescription() {
+    try {
+        let description = document.querySelector(".BreakWord");
+        return stripe_html(description.innerHTML);
+    } catch (e) {
+        return "";
+    }
+}
+
+function getOtherInfo() {
+    try {
+        let info = document.querySelector(".Animn");
+        return stripe_html(info.innerHTML);
+    } catch (e) {
+        return "";
+    }
+}
+
+const getName = () => {
+    try {
+        let name = document.querySelector('span[itemprop="name"]');
+        return name.innerHTML;
+    } catch (e) {
+        return "";
+    }
+}
+
+const stripe_html = (html) => {
+    let doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+}
+
+const getImages = () => {
+    try {
+        let imgs = document.querySelectorAll(".keen-slider__slide img[role='img']");
+        let imgs_url = [];
+        imgs.forEach(img => {
+            let style = img.style.backgroundImage;
+            let url = style.match(/url\("(.+)"\)/)[1];
+            imgs_url.push(url);
+        });
+        return imgs_url;
+    } catch (e) {
+        console.error(e);
+        return [];
+    }
+}
+
+const getKeywords = () => {
+    chrome.storage.sync.get(['keywords'], (result) => {
+        if (result.keywords) {
+            keywords = result.keywords;
+        }
+    });
+};
+
+const checkKeywords = () => {
+    const description = getDescription().toLowerCase()
+    const otherInfo = getOtherInfo().toLowerCase()
+    const name = getName().toLowerCase()
+    return keywords.some(item => description.concat(otherInfo, name).includes(item.toLowerCase()));
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
     if (message.action === 'start') {
+        getKeywords()
         startSwiping();
     } else if (message.action === 'stop') {
         stopSwiping();
--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Options.css';
 
 interface Props {
   title: string;
 }
 
-const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+const Options: React.FC<Props> = ({ title }) => {
+  const [keywords, setKeywords] = useState('');
+
+  // Function to retrieve keywords from Chrome storage
+  const getKeywords = () => {
+    chrome.storage.sync.get(['keywords'], (result) => {
+      if (result.keywords) {
+        setKeywords(result.keywords);
+      }
+    });
+  };
+
+  // Function to save keywords to Chrome storage
+  const saveKeywords = () => {
+    const keywords_array = keywords.split(',')
+    chrome.storage.sync.set({ keywords: keywords_array }, () => {
+      console.log('Keywords saved:', keywords_array);
+    });
+  };
+
+  // Use effect to get keywords when component mounts
+  useEffect(() => {
+    getKeywords();
+  }, []);
+
+  return (
+    <div className="OptionsContainer">
+      <h1>{title} Page</h1>
+      <textarea
+        value={keywords}
+        onChange={(e) => setKeywords(e.target.value)}
+        placeholder="Enter keywords here..."
+      />
+      <button onClick={saveKeywords}>Save Keywords</button>
+    </div>
+  );
 };
 
 export default Options;
